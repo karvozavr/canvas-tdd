fc47332f9b6c665e097191538a9bec58de1fee9c
Dmitrii Abramov <karvozavr@gmail.com>
2021-03-08 18:29:07 +0300

    Add validation for creating canvas

--- application/src/main/kotlin/com/github/karvozavr/canvas/app/command/CreateCanvasCommand.kt
+++ application/src/main/kotlin/com/github/karvozavr/canvas/app/command/CreateCanvasCommand.kt
@@ -1,14 +1,21 @@
 package com.github.karvozavr.canvas.app.command
 
 import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import com.github.karvozavr.canvas.canvas.Canvas
 
 data class CreateCanvasCommand(val width: Int, val height: Int) : Command<CreateCanvasError> {
 
     override fun execute(applicationState: ApplicationState): Either<CreateCanvasError, ApplicationState> {
-        TODO("Not yet implemented")
+        if (width <= 0 && height <= 0) {
+            return InvalidCanvasDimensions.left()
+        }
+
+        return ApplicationState(Canvas.ofSize(width, height)).right()
     }
 }
 
 sealed class CreateCanvasError
 
-object NonPositiveDimensions : CreateCanvasError()
+object InvalidCanvasDimensions : CreateCanvasError()
---
+++ application/src/test/kotlin/com/github/karvozavr/canvas/app/command/CreateCanvasCommandTest.kt
@@ -0,0 +1,32 @@
+package com.github.karvozavr.canvas.app.command
+
+import com.github.karvozavr.canvas.app.emptyApplicationState
+import io.kotest.assertions.arrow.either.shouldBeLeft
+import io.kotest.assertions.arrow.either.shouldBeRight
+import io.kotest.matchers.nulls.shouldNotBeNull
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Assertions.fail
+import org.junit.jupiter.api.Test
+
+internal class CreateCanvasCommandTest {
+
+    @Test
+    fun `should create canvas of given size for no canvas`() {
+        val applicationState = emptyApplicationState()
+        val command = CreateCanvasCommand(20, 10)
+        val result = command.execute(applicationState)
+        result.shouldBeRight()
+        val newState = result.orNull() ?: fail()
+        val canvas = newState.canvas.shouldNotBeNull()
+        canvas.width shouldBe 20
+        canvas.height shouldBe 10
+    }
+
+    @Test
+    fun `should not create canvas of non-positive size`() {
+        val applicationState = emptyApplicationState()
+        val command = CreateCanvasCommand(0, -100)
+        val result = command.execute(applicationState)
+        result shouldBeLeft InvalidCanvasDimensions
+    }
+}
